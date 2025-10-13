cf2825501f4039faf88466f8a80058f39f232c63
Ravi Kiran Palaparthi <ravikiranprk160@gmail.com>
2025-10-13 17:41:26 +0530

    added links to home card images accordingly

--- app/page.jsx
+++ app/page.jsx
@@ -11,24 +11,28 @@ export default function Home() {
         url="/Calculators.png" 
         currentItems={calculators} 
         className="flex flex-col gap-12 md:flex-row p-8 md:p-16 items-center justify-center bg-cyan-100" 
+        sectionUrl="/calculators"
       />
       <HomeCard 
         title="Converters" 
         url="/Converters.png" 
         currentItems={converters} 
         className="flex flex-col gap-12 md:flex-row-reverse p-8 md:p-16 items-center justify-center bg-cyan-100" 
+        sectionUrl="/converters"
       />
       <HomeCard 
         title="Counters" 
         url="/Counters.png" 
         currentItems={counters} 
         className="flex flex-col gap-12 md:flex-row p-8 md:p-16 items-center justify-center bg-cyan-100" 
+        sectionUrl="/counters"
       />
       <HomeCard 
         title="Generators"
         url="/Generators.png"
         currentItems={generators}
         className="flex flex-col gap-12 md:flex-row p-8 md:p-16 items-center justify-center bg-cyan-100"
+        sectionUrl="/generators"
       />
     </main>
   );
--- components/HomeCard.jsx
+++ components/HomeCard.jsx
@@ -1,8 +1,12 @@
-export default function HomeCard({ title, url, currentItems, className }) {
+import Link from "next/link";
+
+export default function HomeCard({ title, url, currentItems, className, sectionUrl }) {
     return (
         <header className={`${className} rounded-xl shadow-md shadow-cyan-600`}>
             <section>
-                <img src={url} alt={title} className="md:max-w-4/5 w-full" />
+                <Link href={sectionUrl}>
+                    <img src={url} alt={title} className="md:max-w-4/5 w-full" />
+                </Link>
             </section>
             <div className="text-gray-950 flex flex-col gap-1 md:max-w-4/5 w-full homecard-text text-center">
                 {
